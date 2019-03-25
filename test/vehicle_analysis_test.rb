ea6e69c66c49a35963d4969e36d516289fd9aef1
Meghan Stang <mstang15@gmail.com>
2019-03-25 10:17:55 -0600

    Add test for vehicle analysis

---
+++ test/vehicle_analysis_test.rb
@@ -0,0 +1,116 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vehicle'
+require './lib/vehicle_analysis'
+require 'stringio'
+require 'o_stream_catcher'
+
+class VehicleTest <Minitest::Test
+
+  def test_it_exists
+    analysis = VehicleAnalysis.new
+
+    assert_instance_of VehicleAnalysis, analysis
+  end
+
+  def test_it_knows_that_pickup_has_four_wheels
+    vehicle_1 = Vehicle.new("pickup", true, true)
+
+    result, stdout, stderr = OStreamCatcher.catch do
+      VehicleAnalysis.new.analyze(vehicle_1)
+    end
+
+    assert stdout.include?("Vehicle has four wheels")
+  end
+
+  def test_it_knows_that_car_has_four_wheels
+    vehicle_1 = Vehicle.new("car", true, true)
+
+    result, stdout, stderr = OStreamCatcher.catch do
+      VehicleAnalysis.new.analyze(vehicle_1)
+    end
+
+    assert stdout.include?("Vehicle has four wheels")
+  end
+
+  def test_it_knows_that_tractor_has_four_wheels
+    vehicle_1 = Vehicle.new("tractor", true, true)
+
+    result, stdout, stderr = OStreamCatcher.catch do
+      VehicleAnalysis.new.analyze(vehicle_1)
+    end
+
+    assert stdout.include?("Vehicle has four wheels")
+  end
+
+  def test_it_knows_that_vehicle_has_four_wheel_drive
+    vehicle_1 = Vehicle.new("tractor", true, true)
+
+    result, stdout, stderr = OStreamCatcher.catch do
+      VehicleAnalysis.new.analyze(vehicle_1)
+    end
+
+    assert stdout.include?("with four wheel drive")
+  end
+
+  def test_it_knows_that_vehicle_does_not_have_four_wheel_drive
+    vehicle_1 = Vehicle.new("pickup", false, true)
+
+    result, stdout, stderr = OStreamCatcher.catch do
+      VehicleAnalysis.new.analyze(vehicle_1)
+    end
+
+    assert_equal false, stdout.include?("with four wheel drive")
+    assert stdout.include?("with two wheel drive")
+  end
+
+  def test_it_knows_that_vehicle_has_big_back_wheels
+    vehicle_1 = Vehicle.new("tractor", false, true)
+
+    result, stdout, stderr = OStreamCatcher.catch do
+      VehicleAnalysis.new.analyze(vehicle_1)
+    end
+
+    assert stdout.include?("with big wheels in the back")
+  end
+
+  def test_it_knows_that_vehicle_does_not_have_big_back_wheels
+    vehicle_1 = Vehicle.new("tractor", false, false)
+
+    result, stdout, stderr = OStreamCatcher.catch do
+      VehicleAnalysis.new.analyze(vehicle_1)
+    end
+
+    assert_equal false, stdout.include?("with big wheels in the back")
+  end
+
+  def test_it_knows_that_all_tractors_have_four_wheels
+    vehicle_1 = Vehicle.new("tractor", false, false)
+
+    result, stdout, stderr = OStreamCatcher.catch do
+      VehicleAnalysis.new.analyze(vehicle_1)
+    end
+
+    assert stdout.include?("Vehicle has four wheels")
+  end
+
+  def test_it_knows_that_all_cars_have_four_wheels
+    vehicle_1 = Vehicle.new("car", false, false)
+
+    result, stdout, stderr = OStreamCatcher.catch do
+      VehicleAnalysis.new.analyze(vehicle_1)
+    end
+
+    assert stdout.include?("Vehicle has four wheels")
+  end
+
+  def test_it_knows_that_all_pickups_have_four_wheels
+    vehicle_1 = Vehicle.new("pickup", false, false)
+
+    result, stdout, stderr = OStreamCatcher.catch do
+      VehicleAnalysis.new.analyze(vehicle_1)
+    end
+
+    assert stdout.include?("Vehicle has four wheels")
+  end
+end
