3f2f184f6c24359b0a10e8ad278261f854c9a6e3
Meghan Stang <mstang15@gmail.com>
2019-03-25 10:17:09 -0600

    Add vehicle analysis and cli

---
+++ lib/vehicle_analysis.rb
@@ -0,0 +1,40 @@
+# vehicle_analysis.rb
+class VehicleAnalysis
+
+  def analyze(vehicle)
+    if vehicle.car?
+      if vehicle.four_wheel_drive? || !vehicle.four_wheel_drive?
+        puts "Vehicle has four wheels "
+        if vehicle.four_wheel_drive?
+          puts "with four wheel drive"
+        else
+          puts "with two wheel drive"
+        end
+        if vehicle.big_back_wheels?
+          puts "with big wheels in the back"
+        end
+      end
+    elsif vehicle.tractor?
+      puts "Vehicle has four wheels "
+      if vehicle.four_wheel_drive?
+        puts "with four wheel drive"
+      else
+        puts "with two wheel drive"
+      end
+      if vehicle.big_back_wheels?
+        puts "with big wheels in the back"
+      end
+    elsif vehicle.pickup?
+      puts "Vehicle has four wheels "
+      if vehicle.four_wheel_drive?
+        puts "with four wheel drive"
+      else
+        puts "with two wheel drive"
+      end
+      if vehicle.big_back_wheels?
+        puts "with big wheels in the back"
+      end
+    end
+  end
+
+end
